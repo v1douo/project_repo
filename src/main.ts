4935125b0f67a889b1353a60dcb5973b3b006e0d
v1douo <705062473@qq.com>
2023-08-10 21:04:02 +0800

    chore: 添加&配置 element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 配置国际化
+// @ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+app.mount('#app')
