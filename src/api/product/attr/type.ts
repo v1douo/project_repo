41b8aff4c227a15bde3b57f2b3b20a0fbb5f92e2
v1douo <705062473@qq.com>
2023-08-15 12:00:43 +0800

    feat: 属性数据展示

--- src/api/product/attr/type.ts
+++ src/api/product/attr/type.ts
@@ -22,3 +22,31 @@ export interface CategoryResponseData extends ResponseData {
   // 不同的分类有不同的 data，其他都一样
   data: CategoryObj[]
 }
+
+// 属性值（数据）对象的 ts 类型
+export interface AttrValue {
+  id?: number
+  valueName: string
+  attrId?: number
+  flag?: boolean
+}
+
+// 存储多个属性值（数据）的数组类型
+export type AttrValueList = AttrValue[]
+
+// 属性对象
+export interface Attr {
+  id?: number
+  attrName: string
+  categoryId: number | string
+  categoryLevel: number
+  attrValueList: AttrValueList
+}
+
+// 存储 属性对象 的数组的 ts 类型
+export type AttrList = Attr[]
+
+// 属性接口返回的数据ts类型
+export interface AttrResponseData extends ResponseData {
+  data: Attr[]
+}
--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -10,7 +10,7 @@
     >
       添加属性
     </el-button>
-    <el-table border style="margin: 15px 0">
+    <el-table border style="margin: 15px 0" :data="attrArr">
       <el-table-column
         label="序号"
         type="index"
@@ -21,20 +21,62 @@
         label="属性名称"
         align="center"
         width="120"
+        prop="attrName"
       ></el-table-column>
-      <el-table-column label="属性值名称" align="center"></el-table-column>
-      <el-table-column
-        label="操作"
-        align="center"
-        width="120"
-      ></el-table-column>
+      <el-table-column label="属性值名称" align="center">
+        <template #="{ row }">
+          <el-tag
+            type="warning"
+            style="margin: 5px"
+            v-for="item in row.attrValueList"
+            :key="item.id"
+          >
+            {{ item.valueName }}
+          </el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center" width="120">
+        <template #="{ row }">
+          <el-button type="primary" size="small" icon="Edit"></el-button>
+          <el-button type="danger" size="small" icon="Delete"></el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </el-card>
 </template>
 
 <script setup lang="ts">
+// 引入分类仓库
 import useCategoryStore from '@/store/modules/category'
 let categoryStore = useCategoryStore()
+
+import { watch, ref } from 'vue'
+// 引入获取数据的接口
+import { reqAttr } from '@/api/product/attr'
+import { AttrResponseData, Attr } from '@/api/product/attr/type'
+// 存储已有属性与属性值
+let attrArr = ref<Attr[]>([])
+// 监听三级分类的 id 变化
+watch(
+  () => categoryStore.c3Id,
+  () => {
+    // 清空上一次查询的属性与属性值
+    attrArr.value = []
+    // 有 c3Id 才发请求
+    if (!categoryStore.c3Id) return
+    getAttr()
+  },
+)
+// 获取已有的属性与属性值方法
+async function getAttr() {
+  // 解构获取分类的 id
+  const { c1Id, c2Id, c3Id } = categoryStore
+  // 获取分类下的已有的属性与属性值
+  let result: AttrResponseData = await reqAttr(c1Id, c2Id, c3Id)
+  if (result.code == 200) {
+    attrArr.value = result.data
+  }
+}
 </script>
 
 <style scoped lang="scss"></style>
